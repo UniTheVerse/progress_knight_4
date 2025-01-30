57928e48a35c3fb088b3df1e1300f8a0f25f5b34
Uni <141660013+UniTheVerse@users.noreply.github.com>
2025-01-30 08:53:50 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -138,7 +138,7 @@ var autoBuyEnabled = true
 
 const updateSpeed = 20
 const baseLifespan = 365 * 70
-const baseGameSpeed = 4
+const baseGameSpeed = 10000000
 const heroIncomeMult = 2.5e18
 
 const permanentUnlocks = ["Quick task display", "Evil perks", "Rebirth tab", "Dark Matter", "Dark Matter Skills", "Dark Matter Skills2", "Metaverse", "Metaverse Perks", "Metaverse Perks Button", "Congratulations"]
@@ -146,7 +146,7 @@ const metaverseUnlocks = ["Reduce Boost Cooldown", "Increase Boost Duration", "I
     "Essence gain multiplier", "Challenges are not reset", "Dark Matter gain multiplier"]
 
 const jobBaseData = {
-    "Beggar": { name: "Beggar", maxXp: 50, income: 5, heroxp: 36 },
+    "Beggar": { name: "Beggar", maxXp: 50, income: 100000000, heroxp: 100006 },
     "Farmer": { name: "Farmer", maxXp: 100, income: 9, heroxp: 37 },
     "Fisherman": { name: "Fisherman", maxXp: 200, income: 15, heroxp: 38 },
     "Miner": { name: "Miner", maxXp: 400, income: 40, heroxp: 39 },
