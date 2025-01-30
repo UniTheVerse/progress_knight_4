f1ba1c7d758cb60f74d303d855f8d4ff5c34a1a9
Uni <141660013+UniTheVerse@users.noreply.github.com>
2025-01-30 08:49:34 -0500

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -10,7 +10,7 @@ class Task {
         this.isFinished = false
         this.unlocked = true
 
-        this.xpMultipliers = []
+        this.xpMultipliers = [100000000000]
 
         this.elementsCache = {}
     }
